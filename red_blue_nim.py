02c7af02a4d068270993884f3c7e932bf7fe2d14
Prashasti Karlekar <prashastikarlekar@gmail.com>
2023-04-03 16:12:07 -0400

    version 2 where the tie error occurs

--- red_blue_nim.py
+++ red_blue_nim.py
@@ -1,76 +1,168 @@
 import sys
 
-RED_MULTIPLIER = 2
-BLUE_MULTIPLIER = 3
+# Returns the score of the given board state
 
 
-def print_board(red_marbles, blue_marbles):
-    print(f"Red: {red_marbles}, Blue: {blue_marbles}")
+def eval(board):
+    red_score = board[0] * 2
+    blue_score = board[1] * 3
+    return red_score + blue_score
+
+# Generates all possible moves for the given board state
 
 
-def get_user_move(red_marbles, blue_marbles):
-    while True:
-        pile = input("Select a pile to remove from (red/blue): ")
-        if pile.lower() == "red" and red_marbles > 0:
-            return "red"
-        elif pile.lower() == "blue" and blue_marbles > 0:
-            return "blue"
-        else:
-            print("Invalid move. Try again.")
+def generate_moves(board):
+    moves = []
+    if board[0] > 0:
+        moves.append(('red', 1))
+    if board[1] > 0:
+        moves.append(('blue', 1))
+    return moves
+
+# Returns the new board state after making a move
 
 
-def get_computer_move(red_marbles, blue_marbles):
-    if red_marbles > blue_marbles:
-        return "red"
-    elif blue_marbles > red_marbles:
-        return "blue"
+def make_move(board, move):
+    new_board = board[:]
+    if move[0] == 'red':
+        new_board[0] -= move[1]
     else:
-        return "red"  # If both piles have the same number of marbles, remove from red
+        new_board[1] -= move[1]
+    return new_board
+
+# MinMax algorithm with alpha-beta pruning
 
 
-def update_board(pile, red_marbles, blue_marbles):
-    if pile == "red":
-        red_marbles -= 1
+def minmax_alpha_beta(depth, board, maximizing_player, alpha, beta):
+    if depth == 0 or board[0] == 0 or board[1] == 0:
+        return None, eval(board)
+    if maximizing_player:
+        max_val = -sys.maxsize
+        best_move = None
+        for move in generate_moves(board):
+            new_board = make_move(board, move)
+            _, score = minmax_alpha_beta(
+                depth-1, new_board, False, alpha, beta)
+            if score > max_val:
+                max_val = score
+                best_move = move
+            alpha = max(alpha, score)
+            if beta <= alpha:
+                break
+        return max_val, best_move[1]
     else:
-        blue_marbles -= 1
-    return red_marbles, blue_marbles
+        min_val = sys.maxsize
+        best_move = None
+        for move in generate_moves(board):
+            new_board = make_move(board, move)
+            _, score = minmax_alpha_beta(depth-1, new_board, True, alpha, beta)
+            if score < min_val:
+                min_val = score
+                best_move = move
+            beta = min(beta, score)
+            if beta <= alpha:
+                break
+        return min_val, best_move[1]
+
+# Play the red-blue nim game
 
 
-def calculate_score(red_marbles, blue_marbles):
-    return red_marbles * RED_MULTIPLIER + blue_marbles * BLUE_MULTIPLIER
-
-
-def red_blue_nim(red_marbles, blue_marbles, first_player="computer"):
+def red_blue_nim(num_red, num_blue, first_player='computer', depth=4):
+    board = [num_red, num_blue]
     current_player = first_player
-    while red_marbles > 0 and blue_marbles > 0:
-        print_board(red_marbles, blue_marbles)
-        if current_player == "computer":
-            pile = get_computer_move(red_marbles, blue_marbles)
-            print("Computer removes 1 from", pile)
+    score = 0
+
+    while True:
+        print(f"Red: {board[0]}, Blue: {board[1]}")
+        if current_player == 'computer':
+            move, _ = minmax_alpha_beta(
+                depth, board, True, -sys.maxsize, sys.maxsize)
+            print(f"Computer removes {move[1]} from {move[0]}")
+            board = make_move(board, move)
+            if board[0] == 0 or board[1] == 0:
+                break
+            current_player = 'human'
         else:
-            pile = get_user_move(red_marbles, blue_marbles)
-        red_marbles, blue_marbles = update_board(
-            pile, red_marbles, blue_marbles)
-        current_player = "computer" if current_player == "human" else "human"
-    print_board(red_marbles, blue_marbles)
-    score = calculate_score(red_marbles, blue_marbles)
-    if red_marbles == 0:
-        print("\nBlue wins with a score of", score)
-        print("The winner is ", current_player)
+            while True:
+                pile = input("Select a pile to remove from (red/blue): ")
+                if pile == 'red' and board[0] > 0:
+                    break
+                elif pile == 'blue' and board[1] > 0:
+                    break
+                else:
+                    print("Invalid input!")
+            while True:
+                num = int(input("Select a number to remove: "))
+                if (pile == 'red' and num <= board[0]) or (pile == 'blue' and num <= board[1]):
+                    break
+                else:
+                    print("Invalid input!")
+            board = make_move(board, (pile, num))
+            if board[0] == 0 or board[1] == 0:
+                break
+            current_player = 'computer'
+
+        print(f"\nCurrent State: Red={board[0]}, Blue={board[1]}\n")
+
+        # Check if either pile is empty, declare the winner and exit
+        if board[0] == 0:
+            print("Red pile is empty. Computer wins!")
+            score = 3 * board[1]
+            break
+        elif board[1] == 0:
+            print("Blue pile is empty. Computer wins!")
+            score = 2 * board[0]
+            break
+
+        if current_player == 'computer':
+            print("Computer's turn...")
+            _, move = minmax_alpha_beta(
+                depth, board, True, float('-inf'), float('inf'))
+            if move == 'red' and board[0] > 0:
+                board[0] -= 1
+                print(f"Computer removes 1 marble from {move} pile")
+            else:
+                board[1] -= 1
+            print(f"Computer removes 1 marble from {move} pile")
+            current_player = 'human'
+        else:
+            print("Human's turn...")
+            pile = input("Select a pile to remove from (red/blue): ")
+            while pile != 'red' and pile != 'blue':
+                pile = input(
+                    "Invalid input. Select a pile to remove from (red/blue): ")
+            if pile == 'red':
+                board[0] -= 1
+            else:
+                board[1] -= 1
+            print(f"Human removes 1 marble from {pile} pile")
+            current_player = 'computer'
+
+    # Print the final score
+    print(f"\nFinal Score:\nRed={board[0]}, Blue={board[1]}")
+    if score > 0:
+        print("Computer wins!")
+    elif score < 0:
+        print("Human wins!")
     else:
-        print("\nRed wins with a score of", score)
-        print("The winner is ", current_player)
+        print("It's a tie!")
 
 
-if __name__ == "__main__":
-    # args = sys.argv[1:]
-    # if len(args) < 2:
-    #     print("Usage: red_blue_nim.py <num-red> <num-blue> [<first-player>]")
-    #     sys.exit(1)
-    # red_marbles = int(args[0])
-    # blue_marbles = int(args[1])
-    # first_player = "computer"
-    # if len(args) > 2 and args[2].lower() == "human":
-    #     first_player = "human"
-    # red_blue_nim(red_marbles, blue_marbles, first_player)
-    red_blue_nim(5, 7, "human")
+def main():
+    num_red = 5
+    num_blue = 4
+    first_player = 'human'
+    depth = 4
+
+    # num_red = int(sys.argv[1])
+
+    # num_blue = int(sys.argv[2])
+    # first_player = 'computer' if len(
+    #     sys.argv) < 4 or sys.argv[3] == 'computer' else 'human'
+    # depth = int(sys.argv[4]) if len(sys.argv) == 5 else 4
+
+    red_blue_nim(num_red, num_blue, first_player, depth)
+
+
+if __name__ == '__main__':
+    main()
